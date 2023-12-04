ac1fe687c55494b0db415c5284e3c51159ad59a4
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-05 00:03:17 +0300

    We need to render botd in army, delcaring a function to render each bot in the army was necessary

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,6 +1,16 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function YourBotArmy() {
-  
+function YourBotArmy({ bots, removeBot, deleteBot }) {
+  // Render BotCards for each bot in the army
+  const renderArmy = bots.map((bot) => (
+    <BotCard
+      key={bot.id}
+      bot={bot}
+      clickEvent={removeBot}
+      deleteBot={deleteBot}
+    />
+  ));
+   
 }
 export default YourBotArmy;
