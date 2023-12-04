4fdc47e969f5c272fac608a8920e70f9f9f37035
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-04 23:50:09 +0300

    Declare variables to store bots, and also need to fetch data from the serve

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
 function BotsPage() {
-  
+  // State variables to store bots and enlisted bots in the army
+  const [bots, setBots] = useState([]);
+  const [yourBotArmy, setYourBotArmy] = useState([]);
+
+  // Fetch bot data from the server on component mount
+  useEffect(() => {
+    fetchData();
+  }, []);
+
 }
 
 export default BotsPage;
