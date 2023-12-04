7abd128fdd40ff39b1ec94ebb929dec8681fd315
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-04 23:58:42 +0300

    Here we are trying to render BotCards, delcaring a function to render each bot in the collection is necessary

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,6 +1,16 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function BotCollection() {
+function BotCollection({ bots, enlistBot, deleteBot }) {
+    // Render BotCards for each bot in the collection
+    const renderBots = bots.map((bot) => (
+      <BotCard
+        key={bot.id}
+        bot={bot}
+        clickEvent={enlistBot}
+        deleteBot={deleteBot}
+      />
+    ));
 
 
 }
